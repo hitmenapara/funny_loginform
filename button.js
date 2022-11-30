79c5a8d00098aaf649ff4ee1c7ec57f155e85456
hit menapara <119275225+hitmenapara@users.noreply.github.com>
2022-11-30 15:21:58 +0530

    Add files via upload

---
+++ button.js
@@ -0,0 +1,20 @@
+const email= document.getElementById("email");
+const password= document.getElementById("password");
+const submitb= document.getElementById("submit");
+
+
+
+
+submitb.addEventListener("mouseover",(button)=>{
+	let email1=email.value;
+	let password1=password.value;
+	let validate=/^([a-zA-Z0-9\._]+)@([a-zA-Z0-9]+).([a-z]+)?/.
+	test(email1)&&/([A-Z])([a-z]+)([0-9]+){8}/.test(password1)
+
+	if (!validate) {
+		button.target.classList.toggle("move");
+	}
+	else{
+		button.target.classList.add("stop");
+	}
+})
