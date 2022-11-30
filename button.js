96d8894048421c1f9173a3a2f8e619ccb3e6a74e
hit menapara <119275225+hitmenapara@users.noreply.github.com>
2022-11-30 15:42:32 +0530

    Update button.js

--- button.js
+++ button.js
@@ -9,7 +9,7 @@ submitb.addEventListener("mouseover",(button)=>{
 	let email1=email.value;
 	let password1=password.value;
 	let validate=/^([a-zA-Z0-9\._]+)@([a-zA-Z0-9]+).([a-z]+)?/.
-	test(email1)&&/([A-Z])([a-z]+)([0-9]+){8}/.test(password1)
+	test(email1)&&/[a-zA-Z0-9]{8}/.test(password1)
 
 	if (!validate) {
 		button.target.classList.toggle("move");
@@ -17,4 +17,5 @@ submitb.addEventListener("mouseover",(button)=>{
 	else{
 		button.target.classList.add("stop");
 	}
-})
+	}
+)
